5eea09214c4eb28be3ea42ede7048cde34dd643f
Snekkers <zir3xx@gmail.com>
2023-08-14 03:24:44 +0200

    Perft refactor

---
+++ Source/Perft.cs
@@ -0,0 +1,53 @@
+﻿namespace Greg
+{
+    internal class Perft
+    {
+        private ulong _currentNps;
+        private ulong _latestNps;
+        private DateTime _latestTimeStamp;
+
+        public void Execute(int depth, Board board, bool splitPerft )
+        {
+            if (splitPerft)
+                PerftInternal( depth, board, true );
+
+            _latestTimeStamp = DateTime.Now;
+            for (int i = 1; i < depth; i++)
+            {
+                Console.WriteLine( $"Depth: {i}, Nodes: {PerftInternal(i, board, false)}, Nps: {_latestNps}" );
+            }
+        }
+
+        public ulong PerftInternal( int depth, Board board, bool splitPerft = false )
+        {
+            if (depth == 0)
+            {
+                _currentNps++;
+                return 1UL;
+            }
+
+            ulong count = 0;
+            var moves = MoveController.GeneratePseudoLegalMoves( board );
+
+            if ((DateTime.Now - _latestTimeStamp).TotalMilliseconds > 1000)
+            {
+                _latestTimeStamp = DateTime.Now;
+                _latestNps = _currentNps;
+                _currentNps = 0;
+            }
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                Board copy = board;
+                if (!copy.MakeMove( moves[i] ))
+                    continue;
+                ulong result = PerftInternal( depth - 1, copy );
+                count += result;
+                if (splitPerft)
+                    Console.WriteLine( $"{moves[i].ToString( board )} - {result}" );
+            }
+
+            return count;
+        }
+    }
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -1,73 +1,14 @@
-﻿using System.Diagnostics;
-
-namespace Greg
+﻿namespace Greg
 {
     internal class Program
     {
-        private static DateTime _startTime;
-        private static ulong _nps;
+        private static Perft _perft = new();
 
         static void Main( string[] args )
         {
             Console.ForegroundColor = ConsoleColor.Gray;
-            Board board = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-
-            _startTime = DateTime.Now;
-            for (int i = 1; i < 8; i++)
-            {
-                ulong result = Perft( i, board, false );
-                Console.WriteLine($"Depth {i}, nodes {result}");
-            }
-
-/*            Console.WriteLine( moves.Length );
-            for (int i = 0; i < moves.Length; i++)
-            {
-                Console.WriteLine( moves[i].ToString(board) );
-            }*/
-
-            while (true)
-            {
-                var move = Console.ReadLine()!;
-            }
-        }
-
-        private static ulong Perft( int depth, Board board, bool first )
-        {
-            if (depth == 0)
-            {
-                _nps++;
-                return 1UL;
-            }
-
-            if ((DateTime.Now - _startTime).TotalMilliseconds > 1000)
-            {
-                _startTime = DateTime.Now;
-                Console.WriteLine( $"nps {_nps}" );
-                _nps = 0;
-            }
-
-            ulong count = 0;
-            var moves = MoveController.GeneratePseudoLegalMoves( board );
-
-            if((DateTime.Now - _startTime).TotalMilliseconds > 1000)
-            {
-                _startTime = DateTime.Now;
-                Console.WriteLine( $"nps {_nps}" );
-                _nps = 0;
-            }
-
-            for (int i = 0; i < moves.Length; i++)
-            {
-                Board copy = board;
-                if (!copy.MakeMove( moves[i] ))
-                    continue;
-                ulong result = Perft( depth - 1, copy, false );
-                count += result;
-                if (first)
-                    Console.WriteLine( $"{moves[i].ToString(board)} - {result}" );
-            }
-
-            return count;
+            Board board = new Board("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
+            _perft.Execute( 10, board, false );
         }
     }
 }
